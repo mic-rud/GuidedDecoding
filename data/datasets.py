598850a7a37e5f3c516ae2b30cf85dac30460692
duraes-antonio <garotoseis@gmail.com>
2023-03-09 21:14:08 -0300

    FEAT: Carregar datasets de forma preguiçosa (lazy item, generator)

--- data/datasets.py
+++ data/datasets.py
@@ -1,39 +1,56 @@
 import os
+import random
 
+import numpy
+import torch
 from torch.utils.data import DataLoader
 
+from config import SEED
 from data.kitti import KITTIDataset
 from data.nyu_reduced import get_NYU_dataset
+
+torch.manual_seed(SEED)
 
 """
 Preparation of dataloaders for Datasets
 """
 
-def get_dataloader(dataset_name, 
+def get_dataloader(dataset_name,
                    path,
-                   split='train', 
+                   split='train',
                    resolution='full',
                    augmentation='alhashim',
-                   interpolation='linear', 
+                   interpolation='linear',
                    batch_size=8,
-                   workers=4, 
+                   workers=4,
                    uncompressed=False):
     if dataset_name == 'kitti':
-        dataset = KITTIDataset(path, 
-                split, 
+        dataset = KITTIDataset(path,
+                split,
                 resolution=resolution)
     elif dataset_name == 'nyu_reduced':
-        dataset = get_NYU_dataset(path, 
-                split, 
-                resolution=resolution, 
+        dataset = get_NYU_dataset(path,
+                split,
+                resolution=resolution,
                 uncompressed=uncompressed)
     else:
         print('Dataset not existant')
         exit(0)
 
-    dataloader = DataLoader(dataset, 
-            batch_size=batch_size, 
-            shuffle=(split=='train'),
-            num_workers=workers, 
-            pin_memory=True)
+    def seed_worker(worker_id):
+        numpy.random.seed(SEED)
+        random.seed(SEED)
+
+    g = torch.Generator()
+    g.manual_seed(SEED)
+
+    dataloader = DataLoader(
+        dataset,
+        batch_size=batch_size,
+        shuffle=(split=='train'),
+        num_workers=workers,
+        pin_memory=True,
+        worker_init_fn=seed_worker,
+        generator=g,
+    )
     return dataloader
--- data/nyu_reduced.py
+++ data/nyu_reduced.py
@@ -1,14 +1,23 @@
-import pandas as pd
+import csv
+import os
+import random
+from io import BytesIO
+from pathlib import Path
+from random import shuffle
+from typing import List, Tuple
+from zipfile import ZipFile
+
 import numpy as np
 import torch
-import os
-from zipfile import ZipFile
-from PIL import Image
-from io import BytesIO
-from torch.utils.data import Dataset, DataLoader
-
 import torchvision.transforms as transforms
+from PIL import Image
+from torch.utils.data import Dataset
+
 from data.transforms import Resize, RandomHorizontalFlip, RandomChannelSwap, ToTensor
+from config import SEED
+
+random.seed(SEED)
+torch.manual_seed(SEED)
 
 resolution_dict = {
     'full' : (480, 640),
@@ -16,15 +25,16 @@ resolution_dict = {
     'mini' : (224, 224)}
 
 class depthDatasetMemory(Dataset):
-    def __init__(self, data, split, nyu2_train, transform=None):
-        self.data, self.nyu_dataset = data, nyu2_train
+    def __init__(self, data: List[Tuple[str, str]], split, transform=None):
+        self.data = data
         self.transform = transform
         self.split = split
 
     def __getitem__(self, idx):
-        sample = self.nyu_dataset[idx]
-        image = Image.open( BytesIO(self.data[sample[0]]) )
-        depth = Image.open( BytesIO(self.data[sample[1]]) )
+        sample = self.data[idx]
+        base_path = '/home/x/Área de Trabalho/GuidedDecoding/data'
+        image = Image.open(Path(base_path, sample[0]))
+        depth = Image.open(Path(base_path, sample[1]))
         image = np.array(image).astype(np.float32)
         depth = np.array(depth).astype(np.float32)
 
@@ -40,7 +50,7 @@ class depthDatasetMemory(Dataset):
         return sample
 
     def __len__(self):
-        return len(self.nyu_dataset)
+        return len(self.data)
 
 class NYU_Testset_Extracted(Dataset):
     def __init__(self, root, resolution='full'):
@@ -66,22 +76,26 @@ class NYU_Testset_Extracted(Dataset):
         return len(self.files)
 
 
-
 class NYU_Testset(Dataset):
     def __init__(self, zip_path):
         input_zip=ZipFile(zip_path)
         data = {name: input_zip.read(name) for name in input_zip.namelist()}
-        
-        self.rgb = torch.from_numpy(np.load(BytesIO(data['eigen_test_rgb.npy']))).type(torch.float32) #Range [0,1]
-        self.depth = torch.from_numpy(np.load(BytesIO(data['eigen_test_depth.npy']))).type(torch.float32) #Range[0, 10]
+        self.data = data
+        #self.rgb = torch.from_numpy(np.load(BytesIO(data['eigen_test_rgb.npy']))).type(torch.float32) #Range [0,1]
+        #self.depth = torch.from_numpy(np.load(BytesIO(data['eigen_test_depth.npy']))).type(torch.float32) #Range[0, 10]
 
     def __getitem__(self, idx):
-        image = self.rgb[idx]
-        depth = self.depth[idx]
+        #image = self.rgb[idx]
+        #depth = self.depth[idx]
+
+        # TODO: Editar para deixar de ser hard coded
+        data_numpy = np.load(BytesIO(self.data['nyu_test_001.npz']))
+        image = torch.from_numpy(data_numpy['image']).type(torch.float32)
+        depth = torch.from_numpy(data_numpy['depth']).type(torch.float32)
         return image, depth
 
     def __len__(self):
-        return len(self.rgb)
+        return len(self.data)
 
 
 
@@ -120,16 +134,21 @@ def val_transform(resolution):
 
 def get_NYU_dataset(zip_path, split, resolution='full', uncompressed=False):
     resolution = resolution_dict[resolution]
-    if split == 'train':
-        data, nyu2_train, nyu2_test = loadZipToMem(zip_path)
 
-        transform = train_transform(resolution)
-        dataset = depthDatasetMemory(data, split, nyu2_train, transform=transform)
-    elif split == 'val':
-        data, nyu2_train, nyu2_test = loadZipToMem(zip_path)
+    if split != 'test':
+        data = read_nyu_csv(zip_path)
+        shuffle(data)
 
-        transform = val_transform(resolution)
-        dataset = depthDatasetMemory(data, split, nyu2_test, transform=transform)
+        if split == 'train':
+            data_train = data
+            transform = train_transform(resolution)
+            dataset = depthDatasetMemory(data_train, split, transform=transform)
+
+        elif split == 'val':
+            data_val = data
+            transform = val_transform(resolution)
+            dataset = depthDatasetMemory(data_val, split, transform=transform)
+
     elif split == 'test':
         if uncompressed:
             dataset = NYU_Testset_Extracted(zip_path)
@@ -137,3 +156,14 @@ def get_NYU_dataset(zip_path, split, resolution='full', uncompressed=False):
             dataset = NYU_Testset(zip_path)
 
     return dataset
+
+
+def read_nyu_csv(csv_file_path) -> List[Tuple[str, str]]:
+    """
+    Lê CSV que relacionada x e y e retona uma lista de pares de paths (x, y)
+    :param csv_file_path: Path do arquivo CSV com o nome de x e y
+    :return: Lista de pares (path input, path ground truth)
+    """
+    with open(csv_file_path, 'r') as file:
+        csv_reader = csv.reader(file, delimiter=',')
+        return [('./' + row[0], './' + row[1]) for row in csv_reader if len(row) > 0]
